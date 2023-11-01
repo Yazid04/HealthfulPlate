a8b2647d562a6d9c62f7278cc37960c42c745e57
Yazid <yazidramadan04@gmail.com>
2023-11-01 13:35:24 +0700

    fixed error of same keys at services.js

--- src/FeaturedServices/data.jsx
+++ src/FeaturedServices/data.jsx
@@ -7,16 +7,8 @@ const data = [
     svgImage: "/assets/picTHREE.png",
     linkAddress: "Search",
   },
-  /*{
-    id: 2,
-    title: "Nutrition Analysis",
-    paragraph:
-      "Paste any food recipe and get detailed nutrition analysis. Understand the nutritional value, calories, macronutrients, and more.",
-    svgImage: "/assets/NutritionAnalysis.png",
-    linkAddress: "Nutrition_Analysis",
-  },*/
   {
-    id: 1,
+    id: 2,
     title: "Weight Goal Calculator",
     paragraph: "Reach Your Ideal Body Weight with Ease! Our user-friendly Weight Goal Calculator offers a personalized recommendations, taking into account your current weight, goal weight, and various factors.",
     svgImage: "/assets/picTWO.png",
